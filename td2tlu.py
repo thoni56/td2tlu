b827ea1906f3701d31f670f31f4f856095abaaaa
Thomas Nilefalk <thomas@junovagen.se>
2019-11-12 22:00:56 +0000

    Hack away to get a rudimentary XML output

--- td2tlu.py
+++ td2tlu.py
@@ -8,13 +8,37 @@ class TimereportConverter():
     def __init__(self):
         pass
 
+    def convert(self, tree):
+        if not tree:
+            return None
+        xml = ET.ElementTree()
+        return xml
+
 
 if (__name__ == "__main__"):
 
     argparser = argparse.ArgumentParser(
         description='Convert XML output from Timeduty to TLU format XML.')
-    argparser.add_argument('file', help='the name of the XML export file')
+    argparser.add_argument(
+        'file', help='name of the XML file from TimeDuty')
 
     args = argparser.parse_args()
 
+    print('Reading...')
     indata = ET.parse(args.file)
+    for setting in indata.iter('setting'):
+        if 'FilterDateFrom' in setting.attrib.values():
+            from_date = setting.attrib['value'].split(' ', 1)[0]
+        if 'FilterDateTo' in setting.attrib.values():
+            to_date = setting.attrib['value'].split(' ', 1)[0]
+
+    print('Writing...')
+    tree = ET.Element('SalaryData')
+    timecodes = ET.SubElement(tree, 'TimeCodes')
+    timecode_sjuk = ET.SubElement(timecodes, 'TimeCode', {
+        'Code': '1', 'TimeCodeName': 'Sjukdom'})
+    timecode_vab = ET.SubElement(timecodes, 'TimeCode', {
+        'Code': '2', 'TimeCodeName': 'VAB'})
+    timecode_semester = ET.SubElement(timecodes, 'TimeCode', {
+        'Code': '3', 'TimeCodeName': 'Semester'})
+    print(ET.tostring(tree))
