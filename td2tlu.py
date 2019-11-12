bd1401d50837bd03fe8765bafe40cf56b52c0c3a
Thomas Nilefalk <thomas@junovagen.se>
2019-11-12 19:07:36 +0100

    Replace sax with etree

--- td2tlu.py
+++ td2tlu.py
@@ -1,25 +1,12 @@
 #! /usr/bin/env python3
 
-import xml.sax
+import xml.etree.ElementTree as ET
 import argparse
 
 
-class TimereportHandler(xml.sax.ContentHandler):
+class TimereportConverter():
     def __init__(self):
-        self.CurrentData = ""
-        self.type = ""
-        self.format = ""
-        self.year = ""
-        self.rating = ""
-        self.stars = ""
-        self.description = ""
-
-    def startElement(self, tag, attributes):
-        self.CurrentData = tag
-        if tag == "movie":
-            print("*****Movie*****")
-            title = attributes["title"]
-            print("Title:", title)
+        pass
 
 
 if (__name__ == "__main__"):
@@ -30,7 +17,4 @@ if (__name__ == "__main__"):
 
     args = argparser.parse_args()
 
-    xmlparser = xml.sax.make_parser()
-    Handler = TimereportHandler()
-    xmlparser.setContentHandler(Handler)
-    xmlparser.parse("simple.xml")
+    indata = ET.parse(args.file)
