11218a469be05d8e53133a5a6ed9f5d66f1b48fc
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2019-12-10 10:27:03 +0100

    Add timecodes for tests

--- td2tlu_test.py
+++ td2tlu_test.py
@@ -6,8 +6,15 @@ from td2tlu import TimereportConverter, User
 
 class TD2TLUTest(unittest.TestCase):
 
+    timecode_table = {
+        'Sjukdom':'1',
+        'VAB':'2',
+        'Semester':'3'
+    }
+
     def convert_and_compare(self, case_name):
-        converter = TimereportConverter(users=[User('anders.bodelius@responsive.se', '1', 'Anders', 'Bodelius')])
+        converter = TimereportConverter(users=[User('anders.bodelius@responsive.se', '1', 'Anders', 'Bodelius')],
+            timecodes=self.timecode_table)
         output = converter.convert(case_name+'.xml', creation_date="2018-04-01")
         output_file = open(case_name+'.output', 'w+')
         print(output, file=output_file)
