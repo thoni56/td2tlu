8432d6db371a5044e46a3e8273ee08b46852e63c
Thomas Nilefalk <thomas@nilefalk.se>
2020-03-08 21:29:19 +0100

    Re-format

--- td2tlu_test.py
+++ td2tlu_test.py
@@ -7,30 +7,30 @@ from td2tlu import Timeduty2TluConverter, User
 class TD2TLUTest(unittest.TestCase):
 
     timecode_table = {
-        'Sjukdom':'1',
-        'VAB':'2',
-        'Semester':'3'
+        'Sjukdom': '1',
+        'VAB': '2',
+        'Semester': '3'
     }
 
     def convert_and_compare(self, case_name):
         converter = Timeduty2TluConverter(users=[User('anders.bodelius@responsive.se', '1', 'Anders', 'Bodelius')],
-            timecodes=self.timecode_table)
-        output = converter.convert_to_tlu(case_name+'.xml', creation_date="2018-04-01")
+                                          timecodes=self.timecode_table)
+        output = converter.convert_to_tlu(
+            case_name+'.xml', creation_date="2018-04-01")
         output_file = open(case_name+'.output', 'w+')
         print(output, file=output_file)
         with open(case_name+'.tlu') as f:
             expected = f.read()
         self.assertEqual(output.strip(), expected.strip())
-            
+
     def test_converts_a_minimal_input_to_tlu(self):
         self.convert_and_compare('minimal')
 
     def test_1_employee_1_registration(self):
         self.convert_and_compare('1_anstalld_1_registrering_franvaro')
-        
+
     def test_1_employee_2_registrations(self):
         self.convert_and_compare('1_anstalld_2_registrering_franvaro')
-    
+
     def test_1_employee_2_mixed_registrations(self):
         self.convert_and_compare('1_anstalld_2_blandade_registreringar')
-
