b730a9a08a678da082f1377febfb12df61f755de
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2020-03-27 11:54:47 +0100

    Add some documentation for tdreader library

--- tdreader.py
+++ tdreader.py
@@ -3,8 +3,11 @@
 
 import lxml.etree as ET
 
-# Returns
-#   from_date, to_date : string YYYY-MM-DD
+# API: extract data from an XML as it is returned from x xml parsers "parse" function
+# Example usage: tdreader.extract_data_from_xml(lxml.etreee.parse(filename))
+# Returns a tuple consisting of:
+#   from_date : string YYYY-MM-DD
+#   to_date : string YYYY-MM-DD
 #   time_rows : list of XML-nodes which all are time registrations
 #   expense_rows : list of XML-nodes which all are expense registrations
 def extract_data_from_xml(indata):
@@ -24,21 +27,33 @@ def extract_data_from_xml(indata):
         'reportrow') if expense_report is not None else []
     return (from_date, to_date, time_rows, expense_rows)
 
+# Filter out registrations for a user (user.id = email as a string)
 def filter_registrations_for_user(user, rows):
     registrations = filter(lambda r: is_row_for_user(
         r, user), rows)
     return list(registrations)
 
+# Filter out registrations for a client (name as a string)
 def filter_registrations_for_client(client, rows):
     registrations = filter(lambda r: is_row_for_client(
         r, client), rows)
     return list(registrations)
 
+# Filter out registrations for a project (name as a string)
 def filter_registrations_for_project(project, rows):
     registrations = filter(lambda r: is_row_for_project(
         r, project), rows)
     return list(registrations)
 
+# Get registered time for a registration
+# Most used is as decimal values, so that is the default
+def get_time(row):
+    return convert_hour_and_minute_as_string_to_fractional_hour(row.find('reportedtime').text)
+
+def get_time_as_string(row):
+    return row.find('reportedtime').text
+
+# Internal
 def is_row_for_user(row, user):
     return row.find('username').text == user
 
@@ -47,16 +62,6 @@ def is_row_for_client(row, client):
 
 def is_row_for_project(row, project):
     return row.find('project').text == project
-
-def get_date(row):
-    return row.find('date').text
-
-# Most used is as decimal values, so that is the default
-def get_time(row):
-    return convert_hour_and_minute_as_string_to_fractional_hour(row.find('reportedtime').text)
-
-def get_time_as_string(row):
-    return row.find('reportedtime').text
 
 def convert_hour_and_minute_as_string_to_fractional_hour(time):
     fields = time.split(":")
